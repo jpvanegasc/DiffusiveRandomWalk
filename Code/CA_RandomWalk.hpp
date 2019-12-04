7076fe63058ad8659acaee266dfc4744730a7202
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2019-12-04 16:28:33 -0500

    Feat: Random Walk propagation on RWLattice class
    Optimization on initialize
    Implements parallel programing

--- Code/CA_RandomWalk.hpp
+++ Code/CA_RandomWalk.hpp
@@ -7,20 +7,25 @@
 
 const int Lx = 200;
 const int Ly = 200;
+const int N = 400;
+
+Crandom ran64(1);
+const double p = 0.5;
 
 class RWLattice{
     private:
         int *coffee = NULL;
     public:
-        RWLattice(void);
+        RWLattice(unsigned long long seed);
         ~RWLattice();
         int get1D(int ix, int iy);
         void initialize(void);
         void propagate(void);
+        bool check(void);
         void save(std::string filename);
 };
 
-RWLattice::RWLattice(void){
+RWLattice::RWLattice(unsigned long long seed){
     coffee = new int[Lx*Ly];
 }
 
@@ -33,22 +38,49 @@ int RWLattice::get1D(int ix, int iy){
 }
 
 void RWLattice::initialize(void){
-    for (int ix=0; ix<Lx; ix++)
-        for (int iy=0; iy<Ly; iy++){
+    // All zeros
+    #pragma opm parallel for
+    for(int i=0; i<Lx*Ly; i++)
+        coffee[i] = 0;
+    // Place molecules
+    for(int ix=90; ix<110; ix++)
+        for(int iy=90; iy<110; iy++){
             int pos = get1D(ix, iy);
-            coffee[pos] = 0;
-            if(ix >= 90 && ix <= 110)
-                if(iy >= 90 && iy <= 110)
-                    coffee[pos] = 1;
+            coffee[pos] = 1;
         }
+    if(check() == false) throw "Bad initialization";
 }
 
 void RWLattice::propagate(void){
     for (int ix=0; ix<Lx; ix++)
         for (int iy=0; iy<Ly; iy++){
             int pos = get1D(ix, iy);
-            coffee[pos] = 0;
+            if(coffee[pos] == 0) continue;
+
+            double r_1 = ran64.r(); double r_2 = ran64.r();
+            if(p > r_1){ // Moves in x
+                if(p > r_2 && ix != Lx-1) coffee[get1D(ix+1, iy)]++; // Right
+                else if(ix != 0) coffee[get1D(ix-1, iy)]++; // Left
+                else if(ix == 0 || ix == Lx-1) continue;
+            }
+            else{ // Moves in y
+                if(p > r_2 && iy != Ly-1) coffee[get1D(ix, iy+1)]++; // Up
+                else if(iy != 0) coffee[get1D(ix, iy)]++; // Down
+                else if(iy == 0 || iy == Ly-1) continue;
+            }
+            coffee[pos]--;
+
         }
+}
+
+bool RWLattice::check(void){
+    int sum = 0;
+    #pragma opm parallel for reduction(+:sum)
+    for(int i=0; i<Lx*Ly; i++)
+        sum += coffee[i];
+    
+    if(sum == N) return true;
+    else return false;
 }
 
 void RWLattice::save(std::string filename){
--- Code/main.cpp
+++ Code/main.cpp
@@ -4,9 +4,15 @@
 #include"CA_RandomWalk.hpp"
 
 int main(void){
-    RWLattice CoffeeMug;
+    RWLattice CoffeeMug(1);
+    int t_max = 1e1;
 
     CoffeeMug.initialize();
+    for (int i=0; i<t_max; i++){
+        CoffeeMug.propagate();
+        //if(CoffeeMug.check() == false) return 1;
+    }
+    
     CoffeeMug.save("coffee.txt");
     
     return 0;
