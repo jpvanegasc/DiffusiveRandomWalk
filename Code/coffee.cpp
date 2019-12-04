433753adc27d420fb1e5c31b6375126d9c65c797
Juan B Benavides <jbbenavidesr@unal.edu.co>
2019-12-03 20:00:25 -0500

    My coffee kinda work.. have to check initial.

--- Code/coffee.cpp
+++ Code/coffee.cpp
@@ -1,46 +1,103 @@
 #include<iostream>
 #include<cmath>
-#include<random>
+#include <random>
 
-//-----GLOBAL CONSTANTS-----
-const int N = 200;
-const int Nmol = 400; 
+//----- Global Constants ----- |
+const double p = 0.5;
+const int Nmol = 400;
+const int v = 1; //Number of steps that a particle move each step.
+const int L = 200;
+const int T = 1;
 
-//-----FUNCTION DECLARATION-----
-void initial(int * Mug);
+//----- Function Declarations ----- |
+void random_step(int *Mol);
+void draw(int *balls);
+void initial(int *mol);
 
-//-----MAIN-----
+//----- MAIN ----- |
 int main(void)
 {
-    int *CoffeeMug = new int[N*N]; 
-    for(int i = 0; i < N; ++i){
-        for(int j = 0; j < N; ++j){
-            CoffeeMug[i*N +j] = 0;
-        }
+    // Initialize
+    int *Mol= new int[Nmol * 2]; // A list with x and y position of all molecules
+    for(int i = 0; i < 2 * Nmol; ++i) {
+        Mol[i] = 0; 
     }
-    initial(CoffeeMug);
+    initial(Mol);
+    for(int t=0; t < T; ++t)
+        random_step(Mol);
 
-    for(int i = 0; i < N; ++i){
-        for(int j = 0; j < N; ++j){
-            std::cout << i - N/2 << "\t" << j - N/2 << "\t" << CoffeeMug[i*N +j] << "\n";
-        }
-        std::cout << std::endl;
-    }
+    draw(Mol);
 
-    delete[] CoffeeMug;
+    delete [] Mol;
 
     return 0;
 }
 
-//-----FUNCTIONS-----
-void initial (int *Mug)
+//----- FUNCTIONS ----- |
+void random_step(int *Mol)
 {
-    //int seed = 1;
-    //std::mt19937 gen(seed);
-    //std::uniform_int_distribution<> dis(-10, 10);
-    for(int xi = N/2-10; xi < N/2 +10; ++xi) {
-        for(int yi = N/2 - 10; yi < N/2 + 10; ++yi)
-        //int xi= dis(gen) + N/2; int yi = dis(gen) + N/2;
-        Mug[xi*N + yi] += 4;
+    /*
+     * Performs a random step.
+     */ 
+    // Init random generator
+    std::random_device rd; //Random seed 
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0, 1);
+
+    // Update
+    for(int i = 0; i < Nmol; ++i) {
+    // ---Select if moving in x or y
+        double r1 = dis(gen);
+        double r2 = dis(gen);
+        if (p > r1){ // Moves in x
+            if (p > r2){ // Moves to right
+                Mol[i*2]++;
+                if (Mol[i*2] == (L/2)) Mol[i*2] = -L/2 + 1;
+            } else { // Moves to left
+                Mol[i*2]--;
+                if (Mol[i*2] == -(L/2)) Mol[i*2] = L/2 - 1;
+            }
+        } else { // Moves in y
+            if (p > r2){ // Moves up
+                Mol[i*2 + 1]++;
+                if (Mol[i*2 + 1] == (L/2)) Mol[i*2 + 1] = -L/2 + 1;
+            } else { // Moves to down
+                Mol[i*2 + 1]--;
+                if (Mol[i*2 + 1] == -(L/2)) Mol[i*2 + 1] = L/2 - 1;
+            }
+        }
     }
+}
+void draw(int *balls)
+{
+    // Init lattice
+    int Lattice[L][L];
+    for (int ii = 0; ii < L; ++ii)
+        for (int jj = 0; jj < L; ++jj)
+            Lattice[ii][jj] = 0;
+    
+    // Insert balls
+    for(int i = 0; i < Nmol; ++i) {
+        Lattice[balls[i*2] + L/2][balls[i*2 + 1] + L/2]++;
+    }
+
+    // Output in pm3d map format
+    for (int ii = 0; ii < L; ++ii){
+        for (int jj = 0; jj < L; ++jj)
+            std::cout << ii - L/2 << "\t" << jj - L/2 << "\t" <<  Lattice[ii][jj] << std::endl;
+        std::cout << std::endl;
+    }
+
+}
+void initial(int *mol)
+{
+    int ri, xi, yi, i = 0;
+    ri = 20; 
+    xi = - ri/2; yi = - ri/2;
+    for(int x = xi; x < (xi + ri); x++)
+        for(int y = yi; y < (yi + ri); y++){
+            mol[2*i] = x;   mol[2*i + 1] = y;
+            i++;  
+        }
+
 }
